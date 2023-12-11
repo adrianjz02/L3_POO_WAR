0be9dedb7ed5c409ed7e7791b9a5e5fb1cc27f07
Nino Fazer <fazernino@gmail.com>
2023-12-11 15:18:44 +0100

    test

--- src/test/java/fr/pantheonsorbonne/miage/engine/CardTest.java
+++ src/test/java/fr/pantheonsorbonne/miage/engine/CardTest.java
@@ -1,24 +1,66 @@
 package fr.pantheonsorbonne.miage.engine;
 
-import org.junit.jupiter.api.Test;  // Assurez-vous d'importer la bonne annotation Test
-import fr.pantheonsorbonne.miage.Cartes.Card;  // Ajoutez cette ligne d'importation
+import org.junit.jupiter.api.Test;
+
+import fr.pantheonsorbonne.miage.Cartes.Card;
 import fr.pantheonsorbonne.miage.Cartes.CardColor;
 import fr.pantheonsorbonne.miage.Cartes.CardValue;
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
 
-    // ...
-
     @Test
     void testIsPetit() {
-        Card petit = new Card(CardColor.ATOUT, CardValue.AS);
-        assertTrue(petit.isPetit());
+        Card petitCard = new Card(CardColor.ATOUT, CardValue.AS);
+        assertTrue(petitCard.isPetit());
 
-        Card nonPetit = new Card(CardColor.ATOUT, CardValue.DEUX);
-        assertFalse(nonPetit.isPetit());
+        Card notPetitCard = new Card(CardColor.ATOUT, CardValue.DIX);
+        assertFalse(notPetitCard.isPetit());
     }
 
-    // ...
+    @Test
+    void testIsBout() {
+        // Ajoutez des tests similaires pour la méthode isBout()
+    }
+
+    @Test
+    void testIsKing() {
+        // Ajoutez des tests similaires pour la méthode isKing()
+    }
+
+    @Test
+    void testIsQueen() {
+        // Ajoutez des tests similaires pour la méthode isQueen()
+    }
+
+    @Test
+    void testIsValet() {
+        // Ajoutez des tests similaires pour la méthode isValet()
+    }
+
+    @Test
+    void testIsCavalier() {
+        // Ajoutez des tests similaires pour la méthode isCavalier()
+    }
+
+    @Test
+    void testIsPetiteCarte() {
+        // Ajoutez des tests similaires pour la méthode isPetiteCarte()
+    }
+
+    @Test
+    void testCardsToString() {
+        // Ajoutez des tests similaires pour la méthode cardsToString()
+    }
+
+    @Test
+    void testCreateTarotDeck() {
+        // Ajoutez des tests similaires pour la méthode createTarotDeck()
+    }
+
+    @Test
+    void testCompareTo() {
+        // Ajoutez des tests similaires pour la méthode compareTo()
+    }
 }
